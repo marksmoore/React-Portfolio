bca9a9c4b69c197f1e0d5a8df1d3829895b01945
Marco <msmo1960@gmail.com>
2021-08-29 15:12:26 -0500

    added Sofa Spud

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,7 @@ function About() {
                 <p>
                 Educated as an Electrical Engineer, I transitioned to Content Management fifteen years ago. While most of my work has been project managment, I have long desired to learn software to round out the skills needed to complete all the tasks associated with my job. Completing the UT Coding Bootcamp will help me work toward completing that.
                 </p>
-            </div>
+            </div><br></br><br></br><br></br>
         </section>
     )
 }
